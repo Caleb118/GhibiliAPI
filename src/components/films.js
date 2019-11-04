8a7f5d05e18e78b19d4aadf9b2db1d4a56f37f90
Caleb <caleb@Calebs-MacBook-Pro.local>
2019-11-04 10:19:40 -0600

    second commit

--- src/components/films.js
+++ src/components/films.js
@@ -6,8 +6,8 @@ const Films = ( props ) => {
         {props.data.map((films) => (
         <div key={films.id}>
         <div className="card">
+        <div class="card-header">{films.title}</div>
             <div className="card-body">
-            <h5 className="card-title">{films.title}</h5>
             <h6 className="card-subtitle mb-2 text-muted">{films.description}</h6>
             <p className="card-text">{films.director}</p>
             </div>
--- src/components/people.js
+++ src/components/people.js
@@ -6,8 +6,8 @@ const People = ( props ) => {
         {props.data.map((people) => (
         <div key={people.id}>
         <div className="card">
+        <div class="card-header">{people.name}</div>
             <div className="card-body">
-            <h5 className="card-title">{people.name}</h5>
             <h6 className="card-subtitle mb-2 text-muted">{people.age}</h6>
             <p className="card-text">{people.gender}</p>
             </div>
